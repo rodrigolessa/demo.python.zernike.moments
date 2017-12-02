4e863578fc43ffec90dc4003dd733a59246ab2e1
Rodrigo <rodrigolsr@gmail.com>
2017-12-02 19:55:42 -0200

    Otimizando scraping

--- web.scraping.images.py
+++ web.scraping.images.py
@@ -1,76 +1,102 @@
+# Libraries for web crawler
 # http://docs.python-requests.org/en/master/
 import requests
+# Managing url
+from urllib.parse import urlparse
 # http://web.stanford.edu/~zlotnick/TextAsData/Web_Scraping_with_Beautiful_Soup.html
 from bs4 import BeautifulSoup
 # https://docs.python.org/3/library/argparse.html
 import argparse
-# Manipulando arquivos
+# Managing windows files
 import os.path
+# I just use for a break, waiting 5 seconds until next request
 import time
+# Regex
+import re
 
-# Construtor de algumentos para execução no console, podendo ser obrigatórios
+# Args constructor for execute on console, podendo ser obrigatórios
 #a = argparse.ArgumentParser()
 #a.add_argument("-p", "--image-list", required=True, help="Path to where the raw HTML file resides")
 #a.add_argument("-s", "--sprites", required=True, help="Path where the sprites will be stored")
 
 #args = vars(a.parse_args())
 
-fileExtension = 'jpg'
+# Initial sets
+baseUrl = 'https://pokemondb.net'
+baseUrlFolder = '/pokedex'
+baseUrlFolderImage = '/artwork'
+imageFolder = 'sprites'
+imageExtension = '.jpg'
+i = 0
 
-# Exemplo com authentication do GitHub
-# r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-# Obtendo o HTML da página
-page = requests.get('https://pokemondb.net/pokedex/national')
+def getHTML(url):
+    # Examples with GitHub authentication # r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    # Navigate to site
+    page = requests.get(url)
+    # print('Confirm id URL is correct!')
+    # print('Status code: %s' % page.status_code)
+    # Try if URL respond
+    if (page.status_code == 200):
+        # print(page.headers['content-type']) # print(page.encoding) # print(page.text) # print(page.content)
+        # Get HTML using BeautifulSoup library # https://imasters.com.br/desenvolvimento/aprendendo-sobre-web-scraping-em-python-utilizando-beautifulsoup/?trace=1519021197&source=single
+        return BeautifulSoup(page.content, 'html.parser')
 
-print('Verificando se o endereço está correto!')
-print('Status code')
-print(page.status_code)
+# Find for 'a' tags  that contains a specific class name
+def getPokeLinks(url):
+    # Get the HTML soup # list(soup.children) # print(soup.prettify())
+    return getHTML(url).find_all('a', class_='ent-name', href=True)
 
-if (page.status_code == 200):
-    print('Headers')
-    print(page.headers['content-type'])
-    # print('Encoding')
-    # print(page.encoding)
-    # print('Text')
-    # print(page.text)
-    # print('Content')
-    # print(page.content)
-    # Analisando o conteúdo da página com a Biblioteca BeautifulSoup
-    # https://imasters.com.br/desenvolvimento/aprendendo-sobre-web-scraping-em-python-utilizando-beautifulsoup/?trace=1519021197&source=single
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # list(soup.children)
-    # Exibindo o conteúdo HTML da página, formatado corretamente
-    # print(soup.prettify())
-    # Find for span tags with Images links and descriptions
-    #infocard = soup.find_all('span', class_='infocard-tall')
-    links = soup.find_all('a', class_='ent-name', href=True)
-    # total de imagens
-    total = len(links)
-    # inicia novo contador
-    i = 0
-    # Loop over all link elements
-    for l in links:
-        # contador
-        i += 1
-        # Tratar os nomes das imagens
-        name = l['href'].replace('/pokedex/', '')
-        # print(l.text)
-        # print(l['href'])
-        # Verificar se a imagem já existe no diretório
-        if (os.path.exists('sprites/%s.%s' % (name, fileExtension))):
-            print("Arquivo %s já existe!" % (name))
+def getPokeName(strLink):
+    return strLink.replace('%s/' % baseUrlFolder, '').lower()
+
+def getPokeImages(strLink):
+    # Confirm the names of images
+    name = getPokeName(strLink)
+    # ! Information
+    print("[%s/%s] downloading %s" % (i, total, name))
+    # construct the URL to download the sprite
+    # imageUrl = "https://img.pokemondb.net/artwork/%s.%s" % (name, imageExtension)
+    # Get all images tags
+    # Using Regex https: // docs.python.org / 3 / library / re.html
+    images = getHTML(baseUrl + strLink).findAll('img', {'src': re.compile('.*artwork.*' + imageExtension)})
+    # Loop over all images
+    for img in images:
+        # Extract image url
+        imgUrl = img['src']
+        imgPath = urlparse(imgUrl).path
+        # Extract image name
+        imgName = imgPath.replace('%s/' % baseUrlFolderImage, '')
+        # Consider only Jpeg images
+        if imgName[-4:] != imageExtension:
             continue
-        # construct the URL to download the sprite
-        print("[%s/%s] downloading %s" % (i, total, name))
-        imageUrl = "https://img.pokemondb.net/artwork/%s.%s" % (name, fileExtension)
-        r = requests.get(imageUrl)
-        # if the status code is not 200, ignore the sprite
+        # If do not exists in image folder
+        if os.path.exists('%s/%s' % (imageFolder, imgName)):
+            # print("The file %s already exists!" % (imgName))
+            continue
+        # Get image
+        r = requests.get(imgUrl)
+        # If the status code is not 200, ignore the sprite
         if r.status_code != 200:
-            print("[x] downloading error %s" % (name))
+            print("[x] downloading error %s" % (imgUrl))
             continue
-        # write the sprite to file
-        f = open("%s/%s.%s" % ('sprites', name.lower(), fileExtension), "wb")
+        # Write the sprite to file
+        f = open("%s/%s" % (imageFolder, imgName), "wb")
         f.write(r.content)
         f.close()
-        # Wait until next link - Foi forçado o cancelamento de uma conexão existente pelo host remoto
-        time.sleep(5)
+        # Wait until next link - Avoid Push out for remote host
+        #time.sleep(5)
+
+##############################################################################
+# Execute web scraping
+# Get all valid links from url
+links = getPokeLinks('%s%s/national' % (baseUrl, baseUrlFolder))
+# Images total
+total = len(links)
+# Loop over all link elements
+for l in links:
+    # increment
+    i += 1
+    # Extract URL from soup link # print(strLink.text) # print(strLink['href'])
+    # TODO: Validate link url
+    # Download all images
+    getPokeImages(l['href'])
